770b4e8f8644116448fc9e828034b91de8fb224b
Evgeniy <zver@gmail.com>
2023-03-03 19:20:47 +0700

    finish

---
+++ app.js
@@ -0,0 +1,21 @@
+function startCards(activeSlide) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+
+    slides.forEach(slide => {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+    
+            slide.classList.add('active');
+        })
+    });
+    
+    function clearActiveClasses() {
+        slides.forEach(slide => {
+            slide.classList.remove('active');
+        })
+    }
+}
+
+startCards(2);
